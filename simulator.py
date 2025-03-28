4a8ff679dc137f73ea196c3aa3e8c45295d561f1
HarshulKr <109364814+HarshulKr@users.noreply.github.com>
2025-03-29 03:54:15 +0530

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -1,11 +1,57 @@
 pc = 0
 arr = [0] * 32
+ram_dict = {
+    addr: 0 for addr in [
+        "0x00010000", "0x00010004", "0x00010008", "0x0001000C",
+        "0x00010010", "0x00010014", "0x00010018", "0x0001001C",
+        "0x00010020", "0x00010024", "0x00010028", "0x0001002C",
+        "0x00010030", "0x00010034", "0x00010038", "0x0001003C",
+        "0x00010040", "0x00010044", "0x00010048", "0x0001004C",
+        "0x00010050", "0x00010054", "0x00010058", "0x0001005C",
+        "0x00010060", "0x00010064", "0x00010068", "0x0001006C",
+        "0x00010070", "0x00010074", "0x00010078", "0x0001007C"
+    ]
+}
+output = "text.txt"
+filename = "testcases.txt"
+with open(output, 'w') as file:
+    file.write("")
+def read_assembly_file(filename):
+    instructions = []
+    
+    with open(filename, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if line and not line.startswith('#'):
+                instructions.append(line)
+    
+    return instructions
+instruction_array = read_assembly_file(filename)
+#print(instruction_array)
 
+def decimal_to_hex(decimal_number):
+    d2h= f"0x{decimal_number:08X}"
+    return d2h
 def printer():
     print(pc, end=" ")
     for i in range(32):
         print(" ", arr[i], end=" ")
     print()
+    
+    with open(output, 'a') as file:
+        file.write(str(pc) + " ")
+        for i in range(32):
+            file.write(" " + str(arr[i]) + " ")
+        file.write("\n")
+
+def sw(imm,rs1,rs2):
+    global pc
+    # print(decimal_to_hex(rs1+imm))
+    # print(ram_dict[decimal_to_hex(rs1+imm)])
+    ram_dict[decimal_to_hex(rs1+imm)]=rs2
+    # print(ram_dict[decimal_to_hex(rs1+imm)])
+    pc+=4
+    printer()
 
 def bne(rs1, rs2, imm):
     global pc
@@ -21,6 +67,12 @@ def beq(rs1, rs2, imm):
         pc += imm
     else:
         pc += 4
+    printer()
+
+def jal(rd, imm):
+    global pc 
+    arr[rd] = pc + 4
+    pc += imm
     printer()
 
 def jal(rd, imm):
@@ -75,15 +127,22 @@ def addi (imm,rs,rd):
 
 print("\npc", end=" ")
 for i in range(32):
-    if(i<10):
-        print(" x",end="0"+str(i))
+    if i < 10:
+        print(" x", end="0" + str(i))
     else:
-        print(" x",end=str(i))
+        print(" x", end=str(i))
 print()
 
+# Test cases
+bne(0, 1, 12)
+beq(0, 1, 12)
+jal(30, -12)
+sw(600,65000,20)
 
-#checks for harshul
-# arr[0]=12
-# bne(0,1,12)
-#beq(0, 1, 12)
-#jal(30,-12)
+#ram output
+f=""
+with open(output, 'a') as file:
+    for key, value in ram_dict.items():
+        f=str(key)+":"+str(value)+"\n"
+        file.write(f)
+        print(f,end="")
