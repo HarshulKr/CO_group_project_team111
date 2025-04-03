cc710f1fa6ea13b15731575cf15a392df3a0411f
HarshulKr <109364814+HarshulKr@users.noreply.github.com>
2025-04-03 22:47:52 +0530

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -1,25 +1,49 @@
 # import sys
 # filename = sys.argv[1]
 # output = sys.argv[2]
+filename = "testcases.txt" 
+output = "text.txt"
 pc=0
 arr=[0]*32
 arr[2]=380
+label_flag=True
+stack_dict = {f"0x{addr:08X}": 0 for addr in range(0x00000100, 0x00000180, 4)}
 ram_dict = {f"0x{addr:08X}": 0 for addr in range(0x00010000, 0x00010080, 4)}
 
-###############################
-#      File Input
-
+#conversions
+#####################################################################################
 def twos_complement1(n: int, bit_length: int = 32) -> str:
     return format((1 << bit_length) + n, f'0{bit_length}b')
-
 def binconv1(n):
     if n<0:
         return twos_complement1(n)
     return format(n, '032b')
+def binary_to_decimal(binary: str) -> int:
+    length = len(binary)
+    is_negative = binary[0] == '1'
+    if is_negative:
+        inverted = ''.join('0' if bit == '1' else '1' for bit in binary)
+        decimal = int(inverted, 2) + 1
+        return -decimal
+    else:
+        return int(binary, 2)
+def decimal_to_hex(n):
+    result = f'{n:x}'
+    result = [i for i in result]
+    for i in range(len(result)):
+        if result[i].isalpha():
+            result[i] = result[i].upper()
+    result = ''.join(result)
+    l= len(result)
+    final=''
+    for i in range(0,8-l):
+        final= final+'0'
+    result='0x'+final+result
+    return result
+#######################################################################################
 
-filename = "testcases.txt" 
-output = "text.txt"
-
+#I/O
+#######################################################################################
 with open(output,'w') as file:
     file.write("")
 
@@ -32,8 +56,18 @@ def read_assembly_file(filename):
                 instructions.append(line)
     return instructions
 
-def decimal_to_hex(decimal_number):
-    return f"0x{decimal_number:08X}"
+def printer2():
+     output2="output_readable.txt"
+     print(pc, end=" ")
+     for i in range(32):
+        print(" ", arr[i], end=" ")
+     print()
+ 
+     with open(output2, 'a') as file:
+         file.write(str(pc) + " ")
+         for i in range(32):
+             file.write(" " + str(arr[i]) + " ")
+         file.write("\n")
 
 def printer():
     with open(output, 'a') as file:
@@ -41,175 +75,224 @@ def printer():
         for i in range(32):
             file.write(f"0b{binconv1(arr[i])} ")  
         file.write("\n")
+    printer2()
+##########################################################################################
+def end():
+    
+    with open(output, 'a') as file:
+        for key, value in ram_dict.items():
+            file.write(f"{key}:0b{binconv1(value)}\n")
 
-#################################################
-#        Instruction Processing
 
-def slicing(s):
-    op=s[-7:]
-    try:
-        if op == "0110011":  # R-type
-            rd=int(s[-12:-7], 2)
+#Main Execution
+
+#########################################################################################
+
+ins = read_assembly_file(filename)
+i=0
+while(i<len(ins)):
+        i=pc//4
+        s=ins[i]
+        op=s[-7:]
+        if(op=="0110011"):
+            rd=int(s[-12:-7],2) 
             func3=s[-15:-12]
             rs1=int(s[-20:-15], 2)
+            #rs1=binary_to_decimal(s[-25:-20])
             rs2=int(s[-25:-20], 2)
+            #rs2=binary_to_decimal(s[-25:-20])
             func7=s[:7]
 
             if func3 == "000":
                 if func7 == "0000000":
-                    rtype(rs1, rs2, rd, 1)  # ADD
+
+                    print("add,rs1,rs2,rd",rs1,rs2,rd)
+                    arr[rd]=arr[rs1]+arr[rs2]
+                    pc+=4
+                    printer()
+
                 elif func7 == "0100000":
-                    rtype(rs1, rs2, rd, 2)  # SUB
-            elif func3 =="010":
-                rtype(rs1, rs2, rd, 5)  # SLT
-            elif func3 == "101":
-                rtype(rs1, rs2, rd, 6)  # SRL
-            elif func3 == "110":
-                rtype(rs1, rs2, rd, 3)  # OR
+
+                    print("sub,rs1,rs2,rd",rs1,rs2,rd)
+                    arr[rd]=arr[rs1]-arr[rs2]
+                    pc+=4
+                    printer()
+            elif func3 =="110":
+                print("or,rs1,rs2,rd",rs1,rs2,rd)
+                
+                arr[rd]=arr[rs1] | arr[rs2]
+                pc+=4
+                printer()
             elif func3 == "111":
-                rtype(rs1, rs2, rd, 4)  # AND
-        
-        elif op == "0000011":  # LW
-            rd = int(s[-12:-7], 2)
-            rs1 = int(s[-20:-15], 2)
-            imm = int(s[:12], 2)
-            lw(rd, rs1, imm)
-        
-        elif op == "0010011":  # ADDI
-            rd = int(s[-12:-7], 2)
-            rs1 = int(s[-20:-15], 2)
-            imm = int(s[:12], 2)
-            addi(imm, rs1, rd)
-        
-        elif op=="1100111":
-            rd = int(s[-8:-13:-1], 2)
-            func3 = s[-13:-16:-1]
-            rs1 = int(s[-16:-21:-1], 2)
-            imm = int(s[-21:-33:-1], 2)
-            
-            jalr(rd,rs1,imm)
-            #jalr
-        
-        elif op=="0100011":
-            imm1 = int(s[-8:-13:-1], 2)
-            func3 = s[-13:-16:-1]
-            rs1 = int(s[-16:-21:-1], 2)
-            rs2 = int(s[-21:-27:-1], 2)
-            imm2 = int(s[-27:-33:-1], 2)
-            final = (imm2 << 5) | imm1
-            sw(final,rs1,rs2)
-        #sw
-        elif op == "1100011":
-            imm1 = s[-8:-13:-1]
-            func3 = s[-13:-16:-1]
-            rs1 = int(s[-16:-21:-1],2)
-            rs2 = int(s[-22:-27:-1],2)
-            imm2 = s[-27:-33:-1]
-
-            imm = imm2[0] + imm1[-1] + imm2[1:] + imm1[:-1] + "0"
-            final_imm = int(imm, 2)
-
-            if func3 == "000":
-                btype(rs1, rs2, final_imm,2)
+                print("srl,rs1,rs2,rd",rs1,rs2,rd)
+                arr[rd]=arr[rs1] & arr[rs2]
+                pc+=4
+                printer()
+            elif func3 == "010":
+                print("slt,rs1,rs2,rd",rs1,rs2,rd)
+                if arr[rs1]<arr[rs2] :     
+                    r=1
+                    arr[rd]=r
+                pc+=4
+                printer()
+            elif func3 == "101":
+                print("and,rs1,rs2,rd",rs1,rs2,rd)
+                r=arr[rs1]>>(arr[rs2]%32)
+                arr[rd]=r
+                pc+=4
+                printer()
+        elif(op=="0000011"):
+            imm = binary_to_decimal(s[:12])
+            rd = int(s[20:25], 2)
+            rs1 = int(s[12:17], 2)
+            address=arr[rs1]+imm 
+            if(decimal_to_hex(address) in ram_dict):
+                arr[rd]=ram_dict.get(decimal_to_hex(address), 0)
             else:
-                btype(rs1, rs2, final_imm,1)
-        
-        elif op=="1101111":
-            rd = int(s[-8:-13:-1], 2)
-            imm=int(s[-13:-33:-1],2)
-            jal(rd,imm)
-            #jal
-    except Exception as e:
-        print(f"Error processing instruction: {s}, Error: {e}")
+                arr[rd]=stack_dict.get(decimal_to_hex(address), 0)  # Load the word
+            pc+=4 
+            print("lw,rd, rs1, imm",rd, rs1, imm)
+            printer()
 
-# Instruction Implementations
+        elif op=="0000001":
+            func7=s[0:8]
+            if func7=="0000000":
+                for i in range(0,32): #rst
+                    arr[i]=0
+                    pc+=4
+                    printer()
+            elif func7=="1000000":
+                printer()
+                end()
+                exit(1)
+            elif func7=="1111111":
+                func3=s[17:20]
+                if func3=="000":
+                    rs1=int(s[12:17])
+                    rs2=int(s[7:12])
+                    rd=int(s[20:25])
+                    arr[rd]=arr[rs1]*arr[rs2]
+                    pc+=4
+                    printer()
+                else:
+                        s=binconv1(arr[rs1])
+                        s1=s[::-1]
+                        arr[rd]=binary_to_decimal(s1)
+                        pc+=4
+                        printer()
+            
+        elif(op=="0010011"):
+            imm = binary_to_decimal(s[:12])
+            rd = int(s[-12:-7], 2)
+            rs1 = int(s[-20:-15], 2)
+            print("addi,imm, rs1, rd",imm, rs1, rd)
+            arr[rd]=arr[rs1]+imm
+            pc+=4
 
-def sw(rs2, rs1, imm):
-    global pc
-    address=arr[rs1] + imm  
-    ram_dict[decimal_to_hex(address)]=arr[rs2]  
-    pc += 4  
-    printer()
+            printer()
+        elif(op=="1100111"):
+            rd = int(s[20:25], 2)
+            func3 = s[17:20]
+            rs1 = int(s[12:17], 2)
+            imm = binary_to_decimal(s[:12])
+            print("jalr,rd,rs1,imm",rd,rs1,imm)
+            if (rd==0):
+                temp =pc 
+                pc= (arr[rs1]+ (imm))
+                #i=(pc//4)-1
+                print(pc)
+                # arr[rd]= temp+4
+                printer()
+                
+            else:
+                temp =pc 
+                pc= (arr[rs1]+ (imm))
+                #i=(pc//4)-1
+                print(pc)
+                arr[rd]= temp+4
+                printer()
+            
+        elif(op=="0100011"):
 
-def lw(rd, rs1, imm):
-    global pc
-    address=arr[rs1]+imm 
-    arr[rd]=ram_dict.get(decimal_to_hex(address), 0)  # Load the word
-    pc+=4  
-    printer()
+            #SW
+            imm1 = s[:7]
+            
+            imm2 = (s[20:25])
+            imm=imm1+imm2
+            imm=binary_to_decimal(imm)
+            rs2=int(s[7:12],2)
+            rs1=int(s[12:17],2)
+            address = arr[rs1] + imm
+            print("sw,address,rs1,rs2,imm",address,rs1,rs2,imm)
+            print(decimal_to_hex(address)) 
+            if(decimal_to_hex(address) in ram_dict):
+                print("ram")
+                ram_dict[decimal_to_hex(address)] = arr[rs2]  
+            else:
+                print("stack")
+                stack_dict[decimal_to_hex(address)] = arr[rs2]  
+            pc += 4  
+            printer()
 
-def jalr(rd, rs1, imm):
-    global pc
-    temp=pc+4
-    pc=(arr[rs1]+imm) & ~1
-    arr[rd]=temp
-    printer()
+
+        elif(op=="1100011"):
+            #print("beq,bne")
+            
+
+            imm1=s[20:25]
+            imm2=s[0:7]
+            imm12=imm2[0]
+            imm5_10=imm2[6:0:-1]
+            imm11=imm1[4]
+            imm1_4=imm1[3:0:-1]+imm1[0]
+            imm="0"+imm1_4+imm5_10+imm11+imm12
+            imm=imm[::-1]
 
 
+            imm = binary_to_decimal(imm)
+            func3 = s[17:20]
+            #print(func3)
+            # rs1=binary_to_decimal(s[-16:-21:-1])
+            # rs2=binary_to_decimal(s[7:12])
+            rs1=int(s[12:17],2)
+            rs2=int(s[7:12],2)
+            if(rs1==0 and rs2==0 and imm==0):
+                printer()
+                end()
+                exit(1)
+            if func3 == "000":
+                print("beq,rs1,rs2,imm",rs1,rs2,imm)
+                if arr[rs1] == arr[rs2]:
+                    pc += imm
+                    #i=(pc//4)-1
+                else:
+                    pc += 4
+                printer()
 
-def btype(rs1,rs2,imm,c):
-    global pc
-    if c==1: #bne
-        if arr[rs1] != arr[rs2]:
-            pc += imm
-        else:
-            pc += 4
-        printer()
-    elif c==2: #beq
-        if arr[rs1] == arr[rs2]:
-            pc += imm
-        else:
-            pc += 4
-        printer()
+            elif func3=="001":
+                print("bne ,rs1,rs2,imm",rs1,rs2,imm)
+                if arr[rs1] != arr[rs2]:
+                    pc += imm
+                    #i=(pc//4)-1
+                else:
+                    pc += 4
+                printer()
 
-def jal(rd, imm):
-    global pc 
-    arr[rd]=pc + 4
-    pc += imm
-    printer()
-
-def rtype(rs1,rs2,rd,c):
-    global pc
-    if c==1: #add
-        arr[rd]=arr[rs1]+arr[rs2]
-        pc +=4
-    elif c==2: #sub
-        arr[rd]=arr[rs1]-arr[rs2]
-        pc+=4
-    elif c==3: #or
-        arr[rd]=arr[rs1] | arr[rs2]
-        pc+=4
-        printer()
-    elif c==4: #and
-        arr[rd]=arr[rs1] & arr[rs2]
-        pc+=4
-        printer()
-    elif c==5: #slt
-        if arr[rs1]<arr[rs2] :     
-            r=1
-            arr[rd]=r
-        pc+=4
-        printer()
-    elif c==6: #srl     
-        r=arr[rs1]>>(arr[rs2]%32)
-        arr[rd]=r
-        pc+=4
-        printer()
-
-def addi (imm,rs,rd):
-    global pc
-    arr[rd]=arr[rs]+imm
-    pc+=4
-    printer()
-
-#################################
-#      Main Execution
-
-ins = read_assembly_file(filename)
-for i in ins:
-    slicing(i)
-
-# RAM Output
-with open(output, 'a') as file:
-    for key, value in ram_dict.items():
-        file.write(f"{key}:0b{binconv1(value)}\n")
+        elif(op=="1101111"):
+            print("jal")
+            rd = int(s[20:25], 2)
+            # imm20=s[0]
+            # imm1_10=s[1:11]
+            # imm11=s[11]
+            # imm12_19=s[19:11:-1]
+            imm=s[0]+s[12:20]+s[11]+s[1:11]+"0"
+            imm=binary_to_decimal(imm)
+            arr[rd]=pc + 4
+            print("rd and imm are",rd,imm)
+            pc=pc+imm
+            #i=(pc//4)-1
+            print(imm)
+            printer()
+            
+        #i+=1               #pc+4
+###################################################################################
