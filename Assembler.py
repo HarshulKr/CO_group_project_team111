ba748c959b2272069f45e3329c2887377fbbafdf
HarshulKr <109364814+HarshulKr@users.noreply.github.com>
2025-12-19 11:23:15 +0530

    Refactor assembler code for clarity and structure
    added automation for checking the kind of output.
    import sys
    if len(sys.argv) >= 3:
        filename = sys.argv[1]
        output = sys.argv[2]
    else:
        filename="input.txt"
        output="output.txt"

--- Assembler.py
+++ Assembler.py
@@ -5,10 +5,16 @@ reg={'zero':'x0','ra':'x1','sp':'x2','gp':'x3','tp':'x4','t0':'x5','t1':'x6','t2
 regad={'x0':'00000','x1':'00001','x2':'00010','x3':'00011','x4':'00100','x5':'00101','x6':'00110','x7':'00111','x8':'01000','x9':'01001','x10':'01010',
 'x11':'01011','x12':'01100','x13':'01101','x14':'01110','x15':'01111','x16':'10000','x17':'10001','x18':'10010','x19':'10011','x20':'10100','x21':'10101','x22':'10110','x23':'10111',
 'x24':'11000','x25':'11001','x26':'11010','x27':'11011','x28':'11100','x29':'11101','x30':'11110','x31':'11111'}
-
 import sys
-filename = sys.argv[1]
-output = sys.argv[2]
+if len(sys.argv) >= 3:
+    filename = sys.argv[1]
+    output = sys.argv[2]
+else:
+    filename="input.txt"
+    output="output.txt"
+#import sys
+#filename = sys.argv[1]
+#output = sys.argv[2]
 
 # filename = "testcases.txt"
 # output = "text.txt"
@@ -306,3 +312,4 @@ with open(output, 'w') as file:
         if f is not None:
                 file.write(f+"\n")
         k+=1
+
